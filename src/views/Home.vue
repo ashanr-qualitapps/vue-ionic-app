0378f4765706017bf33645844ba1b7ba6ce10679
ashanr-qualitapps <ashan.rajapaksha@qualitapps.com>
2024-07-18 05:44:51 +0000

    added vue router and country list shared component

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <CountryList /> <!-- Use the shared component -->
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { CountryList } from '../dist/my-library.es.js'; // Import the shared component
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    CountryList
+  }
+});
+</script>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
